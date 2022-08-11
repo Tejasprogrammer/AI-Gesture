bfeb479be7bf9af244f3e78260e486cd370d7259
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-08-11 09:27:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,64 +1,52 @@
 noseX=0;
 noseY=0;
-difference=0;
-leftwristX=0;
 
-rightwristX=0;
+difference = 0;
 
+rightWristX = 0;
+leftWristX = 0;
 
+  function setup() 
+  {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
-function setup()
-{
-    video = createCapture(VIDEO);
-    video.size( 550 , 500 ) ;
-    canvas=createCanvas( 550 , 500 );
+  canvas = createCanvas(800, 450);
+  canvas.position(560,150);
 
-    canvas.position( 560 , 150 );
-
-    poseNet=ml5.poseNet( video , modelLoaded );
-    poseNet.on('Pose',gotPoses);
-
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded()
+function modelLoaded() 
 {
-    console.log("PoseNet is Initialised");
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
 
 function draw()
 {
-    background('#73ffe1');
-    document.getElementById("square_side").innerHTML="Width and height of a Square will be ="+ difference + "px";
+background('#969A97');
 
-    fill('#308aff');
-
-    stroke('#ff5630');
-
-    square( noseX , noseY , difference );
-
-}
-
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
-        console.log(results);
-
-        noseX=results[0].pose.nose.X
-
-        noseY=results[0].pose.nose.Y
-
-        console.log("noseX="+noseX+"noseY="+noseY);
-
-        leftwristX=results[0].pose.leftwrist.X;
-
-        rightwristX=results[0].pose.rightwrist.X;
-
-        difference=floor(leftwristX-rightwristX)
-      
-        console.log("leftwristX =" + leftwristX + "rightwristX =" + rightwristX + "difference =" + difference );
-
-    }
-
-
+  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
 }
